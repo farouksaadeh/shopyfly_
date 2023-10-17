01900e96ebbbc7ef67645efe8bfb27d18771b9fb
TAASAFA8 <FaroukIzzDaa@hotmail.com>
2023-10-17 15:14:18 +0200

    First spring boot commit

---
+++ src/main/java/ch/shopyfly/shopyfly/ShopyflyApplication.java
@@ -0,0 +1,13 @@
+package ch.shopyfly.shopyfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopyflyApplication.class, args);
+	}
+
+}
