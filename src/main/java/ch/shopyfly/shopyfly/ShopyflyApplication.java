05903d017b30bd44f9bc4145cfe070fe6aa915d9
TAAKIJO2 <Joshuakistler@gmx.ch>
2023-10-17 16:20:46 +0200

    Depencies added

--- src/main/java/ch/shopyfly/shopyfly/ShopyflyApplication.java
+++ src/main/java/ch/shopyfly/shopyfly/ShopyflyApplication.java
@@ -2,12 +2,20 @@ package ch.shopyfly.shopyfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 // Joshua
 @SpringBootApplication
+@RestController
 public class ShopyflyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopyflyApplication.class, args);
 	}
-
+@GetMapping ("/hello")
+	public String hello(@RequestParam(value = "name" , defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+}
 }
