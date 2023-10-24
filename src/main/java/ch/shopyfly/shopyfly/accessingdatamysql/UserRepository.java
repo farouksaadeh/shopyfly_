6ff9ae55e8373cf52ae33244fca6fbd71430b599
TAASAFA8 <FaroukIzzDaa@hotmail.com>
2023-10-24 13:06:38 +0200

    Added (FindByName)

--- src/main/java/ch/shopyfly/shopyfly/accessingdatamysql/UserRepository.java
+++ src/main/java/ch/shopyfly/shopyfly/accessingdatamysql/UserRepository.java
@@ -2,7 +2,11 @@ package ch.shopyfly.shopyfly.accessingdatamysql;
 
 import org.springframework.data.repository.CrudRepository;
 import ch.shopyfly.shopyfly.accessingdatamysql.User;
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-    // ...
+    // ..
+    Optional<User> findByName(String name);
+
 }
